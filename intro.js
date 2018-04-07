d31bcaf36b0b4a680fc750a587023593aefd0b41
daisymae <daisymae@orcadiafarm.com>
2018-04-07 16:40:40 -0500

    New path
    Set a new path to /cities instead of /locations. Modify /locations to
    redirect to /cities. Use 301 to indicate it is a permanent move.
    otherwise, defaults to 302, temporary move.

--- intro.js
+++ intro.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/locations', function(request, response) {
+app.get('/cities', function(request, response) {
 	var cities = '<ul><li>Caspiana</li><li>Indigo</li><li>Paradise</li>';
 	response.send(cities);
+});
+
+app.get('/locations', function(request, response) {
+	response.redirect(301, '/cities');
 });
 
 app.listen(3001, function() {
